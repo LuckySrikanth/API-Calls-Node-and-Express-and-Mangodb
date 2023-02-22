b98e19ec9533353737f9e809392f24169639bb44
LUCKY SRIKANTH <luckysrikanth78@gmail.com>
2023-02-22 12:16:43 +0530

    NEM

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const BrandName = require('./model');
+
+const app = express()
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://srikanth:srikanth@cluster0.jsmfma8.mongodb.net/?retryWrites=true&w=majority").then(() => 
+console.log("DB Connected")
+).catch(err => console.log(err))
+
+
+app.post("/addbrands", async(req, res) => {
+    const {brandname} = req.body ; 
+    try {
+        const newData = new BrandName({brandname})
+        await newData.save()
+        return res.json(await BrandName.find()) 
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.get("/getAllBrands", async(req,res) => {
+    try {
+        const AllData = await BrandName.find()
+        return res.json(AllData);
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.get("/allBrands/:id", async(req, res) => {
+    try {
+        const data = await BrandName.findById(req.params.id)
+        return res.json(data)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.listen(5000, () => console.log("express 5000 port runnning"))
